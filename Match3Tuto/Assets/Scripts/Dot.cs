0c74a611a5a6d0df60ad50503f91f1dd86351bcc
Diego Hartmann <contato.hartmann8@gmail.com>
2021-04-14 16:23:07 -0300

    match funcionando

--- Match3Tuto/Assets/Scripts/Dot.cs
+++ Match3Tuto/Assets/Scripts/Dot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bf4b5db7023780be3d10fd4091c21f8312e3366e11a9358cac7b9269cf6d0bb
-size 3451
+oid sha256:9c111625fad23c8ba3b9498bfa97d434c56c7d94e48f932355f1a13fcc9cc045
+size 4694
