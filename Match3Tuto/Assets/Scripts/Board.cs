b27c3e6fdce7368b76eb5184207bbbcdb97d0a22
Diego Hartmann <contato.hartmann8@gmail.com>
2021-04-15 14:18:15 -0300

    agora nao inicia com matches

--- Match3Tuto/Assets/Scripts/Board.cs
+++ Match3Tuto/Assets/Scripts/Board.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c42d23c7e71785e0948d041440b218d07a6cba7b7d8e7edbdc97ece319f6a3ba
-size 3662
+oid sha256:6e964079cf0573911d70aec6b449274cce0dd3b7a44612d1ce9fdced935ae6dc
+size 4044
--- Match3Tuto/Assets/Scripts/Dot.cs
+++ Match3Tuto/Assets/Scripts/Dot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21daa9036678087f5e12bd99c1dcd093a84a9482cf0fff9e3da21aadddc3e2ce
-size 5405
+oid sha256:9491b80a4c5f3ed8e0265595dc423e9db7b7c876b8111ec34bd326b2c32c7035
+size 5584
