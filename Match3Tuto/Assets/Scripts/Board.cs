1766f7c27660ac081a128db8d4ee1f1bddf97c03
Diego Hartmann <contato.hartmann8@gmail.com>
2021-04-16 11:16:16 -0300

    agora gera bombas verticais ou horizontais em matches especificos

--- Match3Tuto/Assets/Scripts/Board.cs
+++ Match3Tuto/Assets/Scripts/Board.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5acf9397332fa644416d2c3972bc4e86d7170e1bdcc8417c8e02f84c5f2e5781
-size 7580
+oid sha256:42b6d74e54ac4a05660eaf53e5ceafb1497f56afd4bd83bb6ac0b9058f8b0cad
+size 7819
