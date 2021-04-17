7d04c5b538a555597266745237613d58dc93dfbd
Diego Hartmann <contato.hartmann8@gmail.com>
2021-04-17 18:29:07 -0300

    AINDA NAO FUNCIONAL cria espacos em branco

--- Match3Tuto/Assets/Scripts/Board.cs
+++ Match3Tuto/Assets/Scripts/Board.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2b9a283f39c3c9d5de81f261f6ded0ac12a34e7e55a0a357305a6e6fa4bde5f
-size 8303
+oid sha256:27882ea3d19934008e15a8e5baf307bc4d91aaeeb316ece31ece2d173710ff35
+size 9752
