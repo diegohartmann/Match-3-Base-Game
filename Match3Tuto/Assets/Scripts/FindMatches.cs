b6c00d70730c93c1388080e6ffa0a75bca99bc90
Diego Hartmann <contato.hartmann8@gmail.com>
2021-04-17 23:05:47 -0300

    AGORA AS PECAS MATCHADAS LEVAM JUNTO OS VISINHOS IGUAIS A ELA --> match 4/5/6...

--- Match3Tuto/Assets/Scripts/FindMatches.cs
+++ Match3Tuto/Assets/Scripts/FindMatches.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0b63476a60609a3626b6c1ab5f4273a763a93581e9ad18a85db8f3bd310fb49
-size 8562
+oid sha256:b28b91a753f440aefe1ccdba12b7704338faa17a0557cead931ec106b0089cd6
+size 10137
